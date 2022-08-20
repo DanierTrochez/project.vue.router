cdcbfd75d0b6ff1a99c2bfa0fc7776f53c33063f
danier <danier.trochez@misena.edu.co>
2022-08-19 21:29:06 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,24 @@
   <div id="app">
     <nav>
       <router-link to="/">Home</router-link> |
+      <br>
       <router-link to="/about">About</router-link>
+      <br>
+      <router-link to="/InicioView">InicioView</router-link>
+      <br>
+    <router-link to="/dashboar">dashboar</router-link>
+    <br>
+      <router-link to="/create">create</router-link>
+      <br>
+      <router-link to="/index">index</router-link>
+      <br>
+      <router-link to="/ubdate">ubdate</router-link>
+      <br>
+      <router-link to="/dashboart2">dashboart2</router-link>
+      <br>
+      <router-link to="/rol">rol</router-link>
+      <br>
+      <router-link to="/iniciosesion">iniciosesion</router-link>
     </nav>
     <router-view/>
   </div>
---
+++ src/views/InicioView.vue
@@ -0,0 +1,13 @@
+<template>
+ <div>
+     <router-link to="/InicioView">InicioView</router-link>
+           <router-link to="/dashboar">dashboar</router-link>
+      <router-link to="/create">create</router-link>
+      <router-link to="/index">index</router-link>
+      <router-link to="/ubdate">ubdate</router-link>
+      <router-link to="/dashboart2">dashboart2</router-link>
+      <router-link to="/rol">rol</router-link>
+      <router-link to="/iniciosesion">iniciosesion</router-link>
+ </div>
+
+</template> 
